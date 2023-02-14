5872dd525b13db0dee4fae7607b9b534058191b5
Mochammad Qemal Firza <kemalfirza@gmail.com>
2023-02-14 19:30:14 +0700

    hobby func

--- hello.go
+++ hello.go
@@ -1,8 +1,12 @@
 package howtogo
 
 func Hello(name string) string {
-	return "Helloo" + name
+	return "Helloo " + name
 }
 func Profesi(profesi string) string {
-	return "Seorang" + profesi
+	return "Seorang " + profesi
+}
+
+func Hobby(hobby string) string {
+	return "Yang memiliki hobi " + hobby
 }
